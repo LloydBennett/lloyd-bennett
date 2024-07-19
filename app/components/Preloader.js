1d2e9a90c7700d1499fceb6cbf57f0c75374e642
Lloyd-Jamie Bennett <lj_bennett@hotmail.co.uk>
2024-07-19 16:10:22 +0100

    Set up initial animation timeline for preloader

--- app/components/Preloader.js
+++ app/components/Preloader.js
@@ -6,9 +6,29 @@ export default class Preloader extends Components {
      super({
       elements: {
         loader: '[data-loader]', 
-        background: '[data-loader-bg]'
+        background: '[data-preloader-bg]',
+        titleSpans: '[data-title-reveal]',
+        navBar: '[data-nav-bar]'
       }
     })
+    this.start = "M 0 0 V 100 Q 50 100 100 100 V 0 z"
+    this.middle = "M 0 0 V 50 Q 50 0 100 50 V 0 z"
+    this.end = "M 0 0 V 0 Q 50 0 100 0 V 0 z"
+    this.tl = gsap.timeline()
+    this.animate()
     console.log(this.elements)
+  }
+  animate () {
+    this.tl.set(this.elements.background, { attr: { d: this.start }})
+           .to(this.elements.background, { duration: 0.8, attr: { d: this.middle }, ease: "power2.in" }, 0)
+           .to(this.elements.background, { duration: 0.3, attr: { d: this.end }, ease: 'power4', 
+              onComplete: () => { 
+                this.tl.set(this.elements.loader, { display: "none" })
+              }
+            })
+
+    this.tl.fromTo(this.elements.titleSpans, { y: "200%" }, { y: 0, stagger: { each: 0.03, duration: 0.4 }}, "-=0.2")
+    this.tl.fromTo(this.elements.navBar, { opacity: 0 }, { opacity: 1, duration: 0.4 }, "-=0.2")
+
   }
 }
