f1f88b6ddb28438bf9c94e141a2c04ae7682f7d0
Lloyd-Jamie Bennett <lj_bennett@hotmail.co.uk>
2024-07-24 17:05:26 +0100

    Created a create module to be used for both page and components

--- app/classes/Page.js
+++ app/classes/Page.js
@@ -1,33 +1,36 @@
 import GSAP from 'gsap'
+import create from '../utils/Create'
 
 export default class Page {
-  constructor({ elements, element, id }) {
+  constructor({ elements, id }) {
     this.id = id
-    this.selector = element
-    this.selectorChildren = { ...elements }
+    this.selectors = { ...elements }
+    Page.prototype.create = create 
+    this.create()
+    console.log(this)
   }
-  create() {
-    this.element = document.querySelector(this.selector)
-    this.elements = {}
-    // this.selectorChildren.forEach(element => {
+  // create() {
+  //   this.element = document.querySelector(this.selector)
+  //   this.elements = {}
+  //   // this.selectorChildren.forEach(element => {
       
-    // });
-    console.log('Create', this.id, this.element)
-  }
+  //   // });
+  //   console.log('Create', this.id, this.element)
+  // }
   show() {
     return new Promise(resolve => {
-      GSAP.from(this.element, {
-        autoAlpha: 0,
-        onComplete: resolve
-      })
+      // GSAP.from(this.element, {
+      //   autoAlpha: 0,
+      //   onComplete: resolve
+      // })
     })
   }
   hide() {
     return new Promise(resolve => {
-      GSAP.from(this.element, {
-        autoAlpha: 0,
-        onComplete: resolve
-      })
+      // GSAP.from(this.element, {
+      //   autoAlpha: 0,
+      //   onComplete: resolve
+      // })
     })
   }
 }
---
+++ app/utils/Create.js
@@ -0,0 +1,20 @@
+export default function create() {
+  this.elements = {}
+  for(const obj in this.selectors) {
+    let el = this.selectors[obj]
+    if(el instanceof window.HTMLElement || 
+      el instanceof window.NodeList || Array.isArray(el)) {
+      this.elements[obj] = el
+    }
+    else {
+      this.elements[obj] = document.querySelectorAll(el)
+
+      if(this.elements[obj].length == 0) {
+        this.elements[obj] = null
+      } else if (this.elements[obj].length == 1) {
+        this.elements[obj] = document.querySelector(el)
+      }
+    }
+  }
+  console.log(this.elements)
+}
