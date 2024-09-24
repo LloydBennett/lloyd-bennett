f377f730e5d86a6bdb13590eac151357eb93b1d5
Lloyd-Jamie Bennett <lj_bennett@hotmail.co.uk>
2024-09-24 01:09:41 +0100

    Removed animation functionality and created a promise in the preloader to allow us to know when the preloading has finished

--- app/components/Preloader.js
+++ app/components/Preloader.js
@@ -1,82 +1,50 @@
 import Components from 'classes/Components'
-import gsap from 'gsap'
 
 export default class Preloader extends Components {
   constructor(){
      super({
       elements: {
         loader: '[data-loader]', 
-        background: '[data-preloader-bg]',
-        titleSpans: '[data-title-reveal]',
-        heroImage: '[data-hero-image]',
-        heroImgContainer: '[data-hero-image-container]',
-        navBar: '[data-nav-bar]',
-        text: '[data-text-reveal]',
         loaderIcon: '[data-loader-icon]',
         loaderIconCircle: '[data-loader-spinner-circle]',
-        loaderIconSpinner: '[data-loader-spinner]',
-        body: '[data-body]',
-        scrollBtnCircle: '[data-scroll-btn-circle]',
-        scrollBtnArrow: '[data-scroll-btn-arrow]'
-      }
+        loaderIconSpinner: '[data-loader-spinner]'   
+      }      
     })
     
-    this.start = "M 0 0 V 100 Q 50 100 100 100 V 0 z"
-    this.middle = "M 0 0 V 50 Q 50 0 100 50 V 0 z"
-    this.end = "M 0 0 V 0 Q 50 0 100 0 V 0 z"
-
     this.LSPINNER_WIDTH = this.elements.loaderIconSpinner.getBoundingClientRect().width;
     this.LSPINNER_WIDTH_RULE = this.LSPINNER_WIDTH / 10;
     this.LSPINNER_RADIUS = (this.LSPINNER_WIDTH / 2) - (this.LSPINNER_WIDTH_RULE / 2) ;
     this.LSPINNER_CIRCUMFERENCE = 2 * Math.PI * this.LSPINNER_RADIUS;
 
-    this.tl = gsap.timeline()
     this.elements.loaderIconCircle.style.strokeDasharray = this.LSPINNER_CIRCUMFERENCE;
-    this.calculatePageLoadTime(0);
+    this.isLoading = true
   }
 
-  calculatePageLoadTime(progress) {
-    this.updateLoading(progress);
-    progress++;
-
-    let timer = setTimeout(() => {
-      this.calculatePageLoadTime(progress);
-    }, 30);
-
-    if(progress > 100) {
-      clearInterval(timer);
-      this.animate();
-    }
+  calculatePageLoadTime(progress = 0) {
+    return new Promise((resolve) => {
+      let updateProgress = (progress) => {
+        this.updateLoader(progress)
+        progress++
+        
+        if(progress < 100) {
+          setTimeout(() => {
+            updateProgress(progress)
+          }, 30)
+        
+        } else {
+          this.isLoading = false
+          console.log("done!")
+          resolve();
+        }
+      }
+      updateProgress(progress);
+    })
   }
 
-  updateLoading(amount) {
+  updateLoader(amount) {
     let progress = amount / 100;
     let dashoffset = this.LSPINNER_CIRCUMFERENCE * (1 - progress);
     this.elements.loaderIconCircle.style.strokeDashoffset = dashoffset;
-  }
-
-  animate () {
-    this.tl.set(this.elements.background, { attr: { d: this.start }})
-           .to(this.elements.background, { duration: 0.8, attr: { d: this.middle }, ease: "power2.in" }, 0)
-           .to(this.elements.background, { duration: 0.4, attr: { d: this.end }, ease: 'power4', 
-              onComplete: () => {
-                this.tl.set(this.elements.loader, { display: "none" })
-                this.elements.body.classList.remove("no-scrolling")
-              }
-            })
-    this.tl.to(this.elements.loaderIcon, { opacity: 0, duration: 0.4, ease: 'power2.out' }, "-=0.55 icon")
-    this.tl.fromTo(this.elements.titleSpans, { y: "200%" }, { y: 0, duration: 0.65, ease: "power2.out", stagger: { amount: 0.6 }}, "-=0.2")
-    this.tl.fromTo(this.elements.text, { y: "100%" }, { y: 0, duration: 0.4, ease: "power2.out", stagger: (index, target, list) => { return target.dataset.textReveal * 0.1}}, "-=0.2")
-    this.tl.fromTo(this.elements.heroImgContainer, { clipPath: "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)" }, { clipPath: "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)", duration: 0.6, stagger: { amount: 0.4 }, ease: "power2.out" }, "-=1")
-    this.tl.fromTo(this.elements.heroImage, { scale: 2 }, { scale: 1, duration: 0.8, stagger: { amount: 0.4 }, ease: "power2.out" }, "-=1")
-
-
-    if (this.elements.scrollBtnCircle) {
-      this.tl.fromTo(this.elements.scrollBtnCircle, { strokeDashoffset: 2057, strokeDasharray: 2057 }, { strokeDashoffset:0, duration: 1.2, ease: "power2.out" }, "-=1");
-      this.tl.fromTo(this.elements.scrollBtnArrow, { y: -100 }, { y: 0, duration: 0.4, ease: "power2.out" }, "-=0.6");
-    }
-    
-    this.tl.fromTo(this.elements.navBar, { opacity: 0 }, { opacity: 1, duration: 0.8, ease: "power2.out" }, "-=0.2")
   }
 }
 
