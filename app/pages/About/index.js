e3e2d140117a9f9a4862401bfdb15c56d76100af
Lloyd-Jamie Bennett <lj_bennett@hotmail.co.uk>
2024-09-25 01:10:30 +0100

    Removed split text functionality from about page js

--- app/pages/About/index.js
+++ app/pages/About/index.js
@@ -9,60 +9,60 @@ export default class About extends Page {
         transition: '[data-menu-move]'
       }
     })
-    this.text = this.elements.heroText.innerText
-    this.splitLines()
-    this.showLines()
-    this.addEventListeners()
+    //this.text = this.elements.heroText.innerText
+    // this.splitLines()
+    // this.showLines()
+    // this.addEventListeners()
   }
-  addEventListeners() {
-    window.addEventListener('resize', () => {
-      this.splitLines()
-      this.showLines()
-    })
-  }
+  // addEventListeners() {
+  //   window.addEventListener('resize', () => {
+  //     this.splitLines()
+  //     this.showLines()
+  //   })
+  // }
   
-  showLines() {
-    let lines = this.getLines();
+  // showLines() {
+  //   let lines = this.getLines();
 
-    lines.forEach((l, i) => {
-      let lineSpan = document.createElement('span')
-      lineSpan.classList.add('inline-block', 'no-overflow-y', 'white-space')
-      i = i + 1;
+  //   lines.forEach((l, i) => {
+  //     let lineSpan = document.createElement('span')
+  //     lineSpan.classList.add('inline-block', 'no-overflow-y', 'white-space')
+  //     i = i + 1;
       
-      l.forEach((w) => {
-        w.classList.add('inline-block')
-        w.innerHTML += "&nbsp;"
-        w.setAttribute('data-text-reveal', i)
-        lineSpan.appendChild(w)
-        this.elements.heroText.appendChild(lineSpan)
-      })
-    })
-  }
+  //     l.forEach((w) => {
+  //       w.classList.add('inline-block')
+  //       w.innerHTML += "&nbsp;"
+  //       w.setAttribute('data-text-reveal', i)
+  //       lineSpan.appendChild(w)
+  //       this.elements.heroText.appendChild(lineSpan)
+  //     })
+  //   })
+  // }
 
-  splitLines() {
-    let heroText = this.elements.heroText
+  // splitLines() {
+  //   let heroText = this.elements.heroText
     
-    heroText.innerHTML = this.text.split(/\s/).map(function(word) {
-      return '<span>' + word + '</span>'
-    }).join(' ');
-  }
+  //   heroText.innerHTML = this.text.split(/\s/).map(function(word) {
+  //     return '<span>' + word + '</span>'
+  //   }).join(' ');
+  // }
 
-  getLines() {
-    let lines = [];
-    let line
-    let text = this.elements.heroText
-    let words = text.getElementsByTagName('span')
-    let lastTop;
+  // getLines() {
+  //   let lines = [];
+  //   let line
+  //   let text = this.elements.heroText
+  //   let words = text.getElementsByTagName('span')
+  //   let lastTop;
 
-    for (var i = 0; i < words.length; i++) {
-      let word = words[i];
-      if (word.offsetTop != lastTop) {
-        lastTop = word.offsetTop;
-        line = [];
-        lines.push(line);
-      }
-      line.push(word);
-    }
-    return lines;
-  }
+  //   for (var i = 0; i < words.length; i++) {
+  //     let word = words[i];
+  //     if (word.offsetTop != lastTop) {
+  //       lastTop = word.offsetTop;
+  //       line = [];
+  //       lines.push(line);
+  //     }
+  //     line.push(word);
+  //   }
+  //   return lines;
+  // }
 }
