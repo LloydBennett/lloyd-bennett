fe75087c51a82fa2fad4434d000a1e223eebf10c
Lloyd-Jamie Bennett <lj_bennett@hotmail.co.uk>
2024-09-25 01:11:43 +0100

    Rewrote splittext component so that it can entail multiple types of text that need to be split

--- app/components/SplitText.js
+++ app/components/SplitText.js
@@ -10,21 +10,93 @@ export default class Navigation extends Components {
       }
     })
     gsap.registerPlugin(ScrollTrigger)
-    this.splitText()
-    this.animateText()
+    this.splitTextCache = []
+    this.init()
   }
-
-  splitText() {  
-    let text = this.elements.text
-
-    text.innerHTML = text.innerText.split(/\s/).map(function(word) {
-      return '<span>' + word + '</span>'
-    }).join(' ');
+  
+  addEventListeners(textObj) {
+    window.addEventListener('resize', () => {
+      this.splitText(textObj)
+    })
   }
-
-  animateText() {
-    let spans = this.elements.text.querySelectorAll('span')
+  init() {
+    let textContent = [],
+        text = this.elements.text
     
+    if(text instanceof window.NodeList && text.length) {
+      text.forEach((t) => {
+        textContent.push({ text: t.innerText, htmlElem: t })
+      })
+    } else {
+      textContent.push({ text: text.innerText, htmlElem: text })
+    }
+    this.textContent = textContent
+    this.splitText(this.textContent)
+  }
+
+  splitText(text) {
+
+    text.forEach((t) => {
+      t.htmlElem.innerHTML = t.text.split(/\s/).map(function(word) {
+        return '<span>' + word + '</span>'
+      }).join(' ')
+      this.checkTextType(t.htmlElem, t)
+    })
+  }
+
+  checkTextType(text, obj) {
+    let attr = text.getAttribute('data-text')
+
+    if(attr === "line-break") {
+      this.displayLines(text)
+
+      if(this.splitTextCache.indexOf(text) === -1) {
+        this.splitTextCache.push(text)
+        this.addEventListeners([obj])
+      }
+    } else {
+      this.scrollAnimateText(text)
+    }
+  }
+
+  getLines(text) {
+    let lines = [];
+    let line
+    let words = text.children
+    let lastTop;
+
+    for (var i = 0; i < words.length; i++) {
+      let word = words[i];
+      if (word.offsetTop != lastTop) {
+        lastTop = word.offsetTop;
+        line = [];
+        lines.push(line);
+      }
+      line.push(word);
+    }
+    return lines;
+  }
+
+  displayLines(text) {
+    let lines = this.getLines(text);
+
+    lines.forEach((l, i) => {
+      let lineSpan = document.createElement('span')
+      lineSpan.classList.add('inline-block', 'no-overflow-y', 'white-space')
+      i = i + 1;
+      
+      l.forEach((w) => {
+        w.classList.add('inline-block')
+        w.innerHTML += "&nbsp;"
+        w.setAttribute('data-text-reveal', i)
+        lineSpan.appendChild(w)
+        text.appendChild(lineSpan)
+      })
+    })
+  }
+
+  scrollAnimateText(text) {
+    let spans = text.querySelectorAll('span')
     gsap.from(spans, {
       scrollTrigger: {
         trigger: '.type--call-out',
