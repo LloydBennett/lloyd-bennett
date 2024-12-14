607a9a1e71e04e4fcf86480507d2594a5f61e877
Lloyd-Jamie Bennett <lj_bennett@hotmail.co.uk>
2024-12-14 01:08:54 +0000

    Made svg overlay a separate file for easy use

---
+++ app/utils/BackgroundPath.js
@@ -0,0 +1,12 @@
+export const bg = {
+  show: {
+    start: "M 0 0 V 100 Q 50 100 100 100 V 0 z",
+    middle: "M 0 0 V 50 Q 50 0 100 50 V 0 z",
+    end: "M 0 0 V 0 Q 50 0 100 0 V 0 z"
+  },
+  hide: {
+    start: "M 0 100 V 100 Q 50 100 100 100 V 100 z",
+    middle: "M 0 100 V 50 Q 50 0 100 50 V 100 z",
+    end: "M 0 100 V 0 Q 50 0 100 0 V 100 z"
+  }
+}
