a90dc71af8c2bde7af4d3cd5fc95723644a9f99e
Lloyd-Jamie Bennett <lj_bennett@hotmail.co.uk>
2025-09-21 01:06:10 +0100

    Improved navigation functionality

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -1,5 +1,6 @@
 import Components from 'classes/Components'
 import gsap from 'gsap'
+import { scroll } from 'utils/LenisScroll'
 
 export default class Navigation extends Components {
   constructor() {
@@ -15,18 +16,16 @@ export default class Navigation extends Components {
         navLinkSpans: '.nav-menu__list-item [data-page-trigger] span',
         linkTextChar: '[data-link-text] span',
         menuMove: '[data-menu-move]',
-        imgPreview: '[data-nav-menu-preview]',
-        imgPrevBelt: '[data-nav-menu-prev-belt]',
         cursor: '[data-cursor]'
       }
     })
 
     this.isAnimating = false
     this.isMenuOpen = false
-    this.insideProject = false
     this.svgPath = {}
     this.linkSpans = []
-    this.indexCache = 0
+    this.scrollPosition = 0
+    this.lScroll = scroll
     this.addEventListeners()
   }
 
@@ -45,17 +44,74 @@ export default class Navigation extends Components {
   }
 
   intialiseNavLinks() {
-    this.elements.navLinks.forEach((project, i) => {
-      let tag = project.dataset.tag
-            
-      project.addEventListener('mousemove', (e) => {
-        this.handleMouseMove(e)
-        this.showNavPreview()
-        this.moveProjectImg(i);
+    this.elements.navLinks.forEach((link, i) => {
+      const parentItem = link.closest('[data-nav-menu-item]')
+      const underline = parentItem?.querySelector('[data-nav-link-line]')
+      const imgPrev = parentItem?.querySelector('[data-nav-menu-preview]')
+
+      const handleMouseMove = (e, setNow = false) => {
+        const linkRect = link.getBoundingClientRect()
+        const previewRect = imgPrev.getBoundingClientRect()
+
+        const relX = e.clientX - linkRect.left
+        const relY = e.clientY - linkRect.top
+
+        const x = relX - previewRect.width / 2
+        const y = relY - previewRect.height / 2
+
+        if(!setNow) {
+          gsap.to(imgPrev, { 
+            x: x, 
+            y: y,
+            duration: 0.3,
+            ease: "power3.out"
+          })
+        }
+        else {
+          gsap.set(imgPrev, { x, y })
+        }
+      }
+      
+      link.addEventListener('mouseenter', (e) => {
+        this.elements.navLinks.forEach(otherLink => {
+          if (otherLink !== link) {
+            otherLink.classList.add('is-disabled')
+          } else {
+            otherLink.classList.remove('is-disabled')
+            otherLink.classList.add('is-active')
+          }
+        })
+
+        if (underline && !this.isAnimating) {
+          gsap.to(underline, {
+            x: 0,
+            duration: 0.3,
+            ease: "power2.out"
+          })
+        }
+
+        handleMouseMove(e, true)
+        this.showNavPreview(imgPrev)
       })
 
-      project.addEventListener('mouseleave', () => {
-        this.hideNavPreview()
+      link.addEventListener('mouseleave', (e) => {
+        this.elements.navLinks.forEach(otherLink => {
+          otherLink.classList.remove('is-disabled', 'is-active')
+        })
+
+        if (underline) {
+          gsap.to(underline, {
+            x: "-100%",
+            duration: 0.3,
+            ease: "power2.in"
+          })
+        }
+
+        this.hideNavPreview(imgPrev)
+      })
+
+      link.addEventListener('mousemove', (e) => {
+        handleMouseMove(e)
       })
     })
   }
@@ -69,11 +125,12 @@ export default class Navigation extends Components {
     }}
   )
     this.isMenuOpen? this.closeMenu() : this.openMenu()
-    this.elements.body.classList.toggle('no-scrolling')
     this.elements.navMenu.classList.toggle('menu-is-open')
   }
 
   openMenu() {
+    this.lScroll.stop()
+
     let tl = gsap.timeline({
       onComplete: () => {
         this.isAnimating = false
@@ -97,6 +154,12 @@ export default class Navigation extends Components {
   }
 
   closeMenu() {
+    document.body.style.position = ""
+    document.body.style.top = ""
+    document.body.style.width = ""
+
+    this.lScroll.start()
+    
     let tl = gsap.timeline({
       onComplete: () => this.isAnimating = false
     })
@@ -123,54 +186,19 @@ export default class Navigation extends Components {
     tl.to(this.elements.navMenu, { duration: 0.6, opacity: 0, visibility: "hidden" })
   }
 
-  showNavPreview() {
-    if (!this.insideProject) {
-      this.insideProject = true;
-      gsap.to(this.elements.imgPreview, {
-        scale: 1,
-        duration: 0.3,
-        ease: "Power2.out"
-      })
-    }
-  }
-
-  hideNavPreview() {
-    if (this.insideProject) {
-      this.insideProject = false;
-      gsap.to(this.elements.imgPreview, {
-        scale: 0,
-        duration: 0.3,
-        ease: "Power2.out"
-      })
-    }
-  }
-
-  handleMouseMove(e) {
-    const previewRect = this.elements.imgPreview.getBoundingClientRect()
-    const offsetX = previewRect.width / 2
-    const offsetY = previewRect.height / 2
-    const x = e.pageX - offsetX
-    const y = e.pageY - offsetY
-
-    gsap.to(this.elements.imgPreview, { 
-      x: x, 
-      y: y,
-      duration: 0.4,
-      ease: "power3.out"
+  showNavPreview(img) {
+    gsap.to(img, {
+      scale: 1,
+      duration: 0.3,
+      ease: "Power2.out"
     })
   }
 
-  moveProjectImg(i) {
-    let yPos = `-${this.elements.imgPreview.offsetHeight * i}`
-    
-    if(i !== this.indexCache) {
-      gsap.to(this.elements.imgPrevBelt, { 
-        y: yPos,
-        duration: 0.4,
-        ease: "power2.out"
-      })
-
-      this.indexCache = i
-    }
+  hideNavPreview(img) {
+    gsap.to(img, {
+      scale: 0,
+      duration: 0.3,
+      ease: "Power2.out"
+    })
   }
 }
